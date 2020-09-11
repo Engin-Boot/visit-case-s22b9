f89b81de26de99f8c7b14cb8c514d00297d0f938
PseudoNuke <thilakashwin10@gmail.com>
2020-09-11 22:10:10 +0530

    Added Receiver Files

---
+++ Recevier/Recevier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recevier
+{
+    class Recevier
+    {
+        public static List<int> yearList;
+        public static List<int> monthList;
+        public static List<int> dayList;
+        public static List<int> hourList;
+        public static List<int> minuteList;
+        
+        static void Main(string[] args)
+        {
+            TextFileReader reader = new TextFileReader();
+            List<string> textList = reader.ReadTextFile();
+            ListSplitter splittedList = new ListSplitter();
+            splittedList.SplitIntoLists(textList);
+            yearList = splittedList.GetYearList();
+            monthList = splittedList.GetMonthList();
+            dayList = splittedList.GetDayList();
+            hourList = splittedList.GetHourList();
+            minuteList = splittedList.GetMinuteList();
+            Aggregate aggregate = new Aggregate();
+            aggregate.HourlyFootfallsPerDay(2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recevier/TextFileReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recevier
+{
+    class TextFileReader
+    {
+        private List<string> textList = new List<string>();
+        public List<string> ReadTextFile()
+        {
+            string textFilePath = "C:/Users/320104254/source/repos/Bootcamp/Final Sender Recevier/Recevier/formattedOutput.txt";
+            Console.WriteLine("Reading Text File.......");
+            if (File.Exists(textFilePath))
+            {
+                StreamReader inputStreamReader = new StreamReader(textFilePath);
+                string line;
+                while ((line = inputStreamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    this.textList.Add(line);
+                    
+                }
+                
+                inputStreamReader.Close();
+            }
+            else
+            {
+                Console.WriteLine("File Not Found!!");
+            }
+            Console.WriteLine();
+            return this.textList;
+        }
+    }
+}
