52b872c4ee5219dc69ca9b12a97618f63b85419f
TwinkalParmar-16 <twinkal0071@gmail.com>
2020-09-15 13:35:51 +0530

    Files Added

---
+++ Recevier/Aggregator.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recevier
+{
+    public class Aggregator
+    {
+        public int NoOfRows { get; set; }
+        public List<int> IndexList { get; set; }
+        public float Average { get; set; }
+        public float PeakFootFallInAMonth { get; set; }
+        public bool IsAggregatorCalled { get; set; }
+        public bool CheckAverageFootfallsPerHourShownOverADay { get; set; }
+        public bool CheckAverageDailyFootfallsInAWeek { get; set; }
+        public bool CheckPeakDailyFootfallInTheLastMonth { get; set; }
+
+        public Aggregator(TextListSplitter textListSplitter)
+        {
+            IsAggregatorCalled = true;
+            NoOfRows = textListSplitter.YearList.Count;
+            IndexList = new List<int>();
+            Average = 0F;
+            IsAggregatorCalled = true;
+            CheckAverageDailyFootfallsInAWeek = false;
+            CheckAverageFootfallsPerHourShownOverADay = false;
+            CheckPeakDailyFootfallInTheLastMonth = false;
+        }
+        
+        //Average footfalls per hour, shown over a day
+        public void AverageFootfallsPerHourShownOverADay(TextListSplitter textListSplitter,string enteredDate)
+        {
+                
+                enteredDate = enteredDate.Trim();
+                string[] ddMmYyDate = enteredDate.Split('/');
+                int day = Int16.Parse(ddMmYyDate[0]);
+                int month = Int16.Parse(ddMmYyDate[1]);
+                int year = Int16.Parse(ddMmYyDate[2]);
+                AggregatorSupporter aggregatorSupporter = new AggregatorSupporter();
+                IndexList = aggregatorSupporter.GetIndex(day, month, year, textListSplitter, NoOfRows);
+                List<int> finalHourList = new List<int>();
+                for (int index = 0; index < IndexList.Count; index++)
+                {
+                    finalHourList.Add(textListSplitter.HourList[IndexList[index]]);
+                } 
+                //printing to console
+                Console.WriteLine("------------Average FootFall Per Hour-----------------");
+                Console.WriteLine("Given Date : {0}", enteredDate);
+                Console.WriteLine("_____________________________");
+                Console.WriteLine("Hour   Count");
+                var valueVsFreq = ShowFreq(finalHourList);
+                Console.WriteLine("Average FootFall Count : {0}", Average);
+                Console.WriteLine("_____________________________");
+                //Prinitng to file
+                string contentToBePrinted = "";
+                contentToBePrinted += "Given Date,"+ enteredDate+"\n";
+                contentToBePrinted += "Hour,Count\n";
+                contentToBePrinted += valueVsFreq;
+                contentToBePrinted += "Average FootFall Count,"+Average+"\n"; 
+                CsvFileWriter csvFileWriter= new CsvFileWriter();
+                csvFileWriter.writeToCsvFile(contentToBePrinted, "AverageFootfallsPerHourShownOverADay");
+                CheckAverageFootfallsPerHourShownOverADay = true;
+        }
+
+        //Average daily footfalls in a week
+        public void AverageDailyFootfallsInAWeek(TextListSplitter textListSplitter,string enteredDate)
+        {
+                
+                enteredDate = enteredDate.Trim();
+                string[] ddMmYyDate = enteredDate.Split('/');
+                int day = Int16.Parse(ddMmYyDate[0]);
+                int month = Int16.Parse(ddMmYyDate[1]);
+                int year = Int16.Parse(ddMmYyDate[2]);
+                DateTime currentDate = Convert.ToDateTime(enteredDate);
+                Dictionary<DateTime, int> dayVsCount = new Dictionary<DateTime, int>();
+                AggregatorSupporter aggregatorSupporter = new AggregatorSupporter();
+                dayVsCount.Add(currentDate,
+                    aggregatorSupporter.GetCountPerDay((int) currentDate.Day, (int) currentDate.Month,
+                        (int) currentDate.Year, textListSplitter, NoOfRows));
+                for (int startDay = 1; startDay <= 6; startDay++)
+                {
+                    currentDate = currentDate.AddDays(1);
+                    dayVsCount.Add(currentDate,
+                        aggregatorSupporter.GetCountPerDay((int) currentDate.Day, (int) currentDate.Month,
+                            (int) currentDate.Year, textListSplitter, NoOfRows));
+                }
+                Console.WriteLine("---------Average daily footfalls in a week---------------");
+                Console.WriteLine("Given Date : {0}", enteredDate);
+                Console.WriteLine("_____________________________");
+                Console.WriteLine("Day            Count");
+                Average = 0;
+                //Prinitng to file
+                string contentToBePrinted = "";
+                contentToBePrinted += "Given Date," + enteredDate + "\n";
+                contentToBePrinted += "Day,Count\n";
+                foreach (KeyValuePair<DateTime, int> dayAndFreq in dayVsCount)
+                {
+                    Console.WriteLine("{0}        {1}", dayAndFreq.Key.ToString("dd/MM/yyyy"), dayAndFreq.Value);
+                    Average += (float) dayAndFreq.Value;
+                    contentToBePrinted += dayAndFreq.Key.ToString("dd/MM/yyyy") + "," + dayAndFreq.Value + "\n";
+                }
+                Console.WriteLine("Average FootFall Count : {0}", Average/7);
+                contentToBePrinted += "Average FootFall Count," + (Average / 7) + "\n";
+                Console.WriteLine("_____________________________");
+               
+                CsvFileWriter csvFileWriter = new CsvFileWriter();
+                csvFileWriter.writeToCsvFile(contentToBePrinted, "AverageDailyFootfallsInAWeek");
+                CheckAverageDailyFootfallsInAWeek = true;
+        }
+
+        //Peak daily footfall in the last month
+        public void PeakDailyFootfallInTheLastMonth(TextListSplitter textListSplitter,string monthYear)
+        {
+                
+                monthYear = monthYear.Trim();
+                string[] monthNYear = monthYear.Split(' ');
+                int enteredMonth=Int32.Parse(monthNYear[0]);
+                int enteredYear = Int32.Parse(monthNYear[1]);
+                AggregatorSupporter aggregatorSupporter = new AggregatorSupporter();
+                IndexList=aggregatorSupporter.GetListOfGivenMonthAndYear(enteredMonth, enteredYear, textListSplitter,NoOfRows);
+                //IndexList:which matches with given month and year
+                Console.WriteLine(IndexList.Count);
+                List<int> finalDayList = new List<int>();
+                for (int index = 0; index < IndexList.Count; index++)
+                {
+                    finalDayList.Add(textListSplitter.DayList[IndexList[index]]);
+                }
+                Console.WriteLine("-------Peak Daily Footfall In The  Month---------");
+                Console.WriteLine("Given Month : {0}", enteredMonth);
+                Console.WriteLine("Given Year : {0}", enteredYear);
+                Console.WriteLine("_____________________________");
+                Console.WriteLine("Day   Count");
+                var valShowFreq = ShowFreq(finalDayList);
+                Console.WriteLine("Average FootFall Count : {0}", Average);
+                Console.WriteLine("Peak FootFall Count : {0}", PeakFootFallInAMonth);
+                Console.WriteLine("_____________________________");
+
+                //Prinitng to file
+                string contentToBePrinted = "";
+                contentToBePrinted += "Given Month," + enteredMonth + "\n";
+                contentToBePrinted+= "Given Year,"+enteredYear + "\n";
+                contentToBePrinted += "Day,Count" + "\n";
+                contentToBePrinted += valShowFreq;
+                contentToBePrinted+= "Average FootFall Count,"+ Average+"\n";
+                contentToBePrinted += "Peak FootFall Count," + PeakFootFallInAMonth + "\n";
+                CsvFileWriter csvFileWriter = new CsvFileWriter();
+                csvFileWriter.writeToCsvFile(contentToBePrinted, "PeakDailyFootfallInTheLastMonth");
+                CheckPeakDailyFootfallInTheLastMonth = true;
+        }
+
+        public string ShowFreq(List<int> inputList)
+        {
+                int n = inputList.Count;
+                PeakFootFallInAMonth = 0;
+                int sum = 0;
+                int totalRows = 0;
+                string valueVsFreq = "";
+                bool[] visited = new bool[n];
+                Dictionary<int, int> map = new Dictionary<int, int>();
+                for (int i = 0; i < n; i++)
+                {
+                    if (visited[i] == true)
+                        continue;
+                    int count = 1;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (inputList[i] == inputList[j])
+                        {
+                            visited[j] = true;
+                            count++;
+                        }
+                    }
+                    Console.WriteLine(inputList[i] + " " + count);
+                    valueVsFreq += inputList[i] + "," + count+"\n";
+                    map.Add(inputList[i], count);
+                    if (PeakFootFallInAMonth < count)
+                    {
+                        PeakFootFallInAMonth = count;
+                    }
+                    totalRows++;
+                    sum += count;
+                }
+                try
+                {
+                    Average = sum / totalRows;
+                   
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e + " Check Aggregator->ShowFreq");
+                    throw;
+                }
+                return valueVsFreq;
+        }
+    }
+}
+
---
+++ Recevier/TextListSplitter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Recevier
+{
+    public class TextListSplitter
+    {
+        public List<int> DayList { get; set; }
+        public List<int> MonthList { get; set; }
+        public List<int> YearList { get; set; }
+        public List<int> HourList { get; set; }
+        public List<int> MinuteList { get; set; }
+        public bool IsListSpliited { get; set; }
+        public TextListSplitter(List<string> textList)
+        {
+            IsListSpliited = false;
+            this.DayList=new List<int>();
+            this.MonthList=new List<int>();
+            this.YearList=new List<int>();
+            this.HourList=new List<int>();
+            this.MinuteList=new List<int>();
+            this.SplitTextList(textList);
+            IsListSpliited = true;
+        }
+        private List<string> SplitStringIntoValues(string values)
+        {
+            string[] listEntry = values.Split(' ');
+            List<string> splittedValueList = new List<string>();
+            try
+            {
+                for (int index = 0; index < listEntry.Length; index++)
+                    splittedValueList.Add(listEntry[index]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e+" Check TextListSplitter->SplitStringIntoValue");
+                throw;
+            }
+            return splittedValueList;
+        }
+        private string getValueString(string inputString)
+        {
+            string[] datetypeAndValueString = inputString.Split(':');
+            try
+            {
+                string valueString = datetypeAndValueString[1];
+                valueString = valueString.Trim();
+                return valueString;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e+"  Check TextListSplitter->getValueString");
+                throw;
+            }
+        }
+        private void SplitTextList(List<string> textList)
+        {
+            try
+            {
+                string valueString = getValueString(textList[1]);
+                this.DayList = SplitStringIntoValues(valueString).ConvertAll(int.Parse);
+                valueString = getValueString(textList[2]);
+                this.MonthList = SplitStringIntoValues(valueString).ConvertAll(int.Parse);
+                valueString = getValueString(textList[3]);
+                this.YearList = SplitStringIntoValues(valueString).ConvertAll(int.Parse);
+                valueString = getValueString(textList[4]);
+                this.HourList = SplitStringIntoValues(valueString).ConvertAll(int.Parse);
+                valueString = getValueString(textList[5]);
+                this.MinuteList = SplitStringIntoValues(valueString).ConvertAll(int.Parse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e+" Check TextSplitter");
+                throw;
+            }
+        }
+    }
+}
