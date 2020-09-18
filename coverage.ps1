bdbc1826b639da020b50fad40290ed44aa1ac09b
Twinkal Parmar <66124568+TwinkalParmar-16@users.noreply.github.com>
2020-09-18 15:16:15 +0530

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -11,7 +11,7 @@ function WriteXmlToScreen ([xml]$xml)
     Write-Output $StringWriter.ToString();
 }
 
-$report = Get-Content -Path UnitTestProject\TestResults\*\coverage.cobertura.xml | Out-String
+$report = Get-Content -Path Sender.Tests\TestResults\*\coverage.cobertura.xml | Out-String
 Write-Host "---------------------------------"
 Write-Host "Code Coverage report ..." 
 Write-Host "---------------------------------"
